0060fd895263e8408cccaf32f0b8324dccfd0592
Krisha Joshi <krishasjoshi@gmail.com>
2024-04-07 09:23:15 +0530

    Edited Login Page

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from './app/(authenticate)/Login'
+import Register from './app/(authenticate)/Register';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }} // Hide the header for Login screen
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }} // Hide the header for Register screen
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
